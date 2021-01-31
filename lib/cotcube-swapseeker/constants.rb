cd585110ac08e77a15f9e5d14621862304485673
Benjamin L. Tischendorf <donkeybridge@jtown.eu>
2021-01-31 14:00:50 +0000

    added init and constants

---
+++ lib/cotcube-swapseeker/constants.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+module Cotcube
+  module SwapSeeker
+
+    SYMBOL_EXAMPLES = [
+      { id: "13874U", symbol: "ET", ticksize: 0.25, power: 1.25, months: "HMUZ", bcf: 1.0, reports: "LF", name: "S&P 500 MICRO" },
+      { id: "209747", symbol: "NM", ticksize: 0.25, power: 0.5,  monhts: "HMUZ", bcf: 1.0, reports: "LF", name: "NASDAQ 100 MICRO" }
+    ].freeze
+
+    COLORS         = [ :light_red, :light_yellow, :light_green, :red, :yellow, :green, :cyan, :magenta, :blue ]
+    MONTH_COLOURS  = { 'F' => :cyan,  'G' => :green,   'H' => :light_green,
+                       'J' => :blue,  'K' => :yellow,  'M' => :light_yellow,
+                       'N' => :cyan,  'Q' => :magenta, 'U' => :light_magenta,
+                       'V' => :blue,  'X' => :red,     'Z' => :light_red }.freeze
+
+  end
+end
---
+++ lib/cotcube-swapseeker/init.rb
@@ -0,0 +1,78 @@
+# frozen_string_literal: true
+
+module Cotcube
+  module SwapSeeker
+
+
+    def symbols(config: init, type: nil)
+      if config[:symbols_file].nil?
+        SYMBOL_EXAMPLES
+      else
+        CSV
+          .read(config[:symbols_file], headers: %i{ id symbol ticksize power months type bcf reports name})
+          .map{|row| row.to_h }
+          .map{|row| [ :ticksize, :power, :bcf ].each {|z| row[z] = row[z].to_f}; row }
+          .reject{|row| row[:id].nil? }
+          .tap{|all| all.select!{|x| x[:type] == type} unless type.nil? }
+      end
+    end
+
+    def config_prefix
+      os       = Gem::Platform.local.os
+      case os
+      when 'linux'
+        ''
+      when 'freebsd'
+        '/usr/local'
+      else
+        raise RuntimeError, 'unknown architecture'
+      end
+    end
+
+    def config_path
+      config_prefix + '/etc/cotcube'
+    end
+
+    def init(config_file_name: 'swapseeker.yml', debug: false)
+      name = 'swapseeker'
+      config_file = config_path + "/#{config_file_name}"
+
+      if File.exist?(config_file)
+        config      = YAML.load(File.read config_file).transform_keys(&:to_sym)
+      else
+        config      = {} 
+      end
+
+      defaults = { 
+        data_path: config_prefix + '/var/cotcube/' + name,
+      }
+
+
+      config = defaults.merge(config)
+      puts "CONFIG is '#{config}'" if debug
+
+
+      # part 2 of init process: Prepare directories
+
+      save_create_directory = lambda do |directory_name|
+        unless Dir.exist?(directory_name)
+          begin
+            `mkdir -p #{directory_name}`
+            unless $?.exitstatus.zero?
+              puts "Missing permissions to create or access '#{directory_name}', please clarify manually"
+              exit 1 unless defined?(IRB)
+            end
+          rescue 
+            puts "Missing permissions to create or access '#{directory_name}', please clarify manually"
+            exit 1 unless defined?(IRB)
+          end
+        end
+      end
+      symbols(config: config).map{|x| x[:symbol]}.each do |sym|
+        dir = "#{config[:data_path]}/#{sym}"
+        save_create_directory.call(dir)
+      end
+    end
+  end
+end
+
