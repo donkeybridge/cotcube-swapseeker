176e0810b731a9fe6c3e9cf9508e8853746fc69e
Benjamin L. Tischendorf <donkeybridge@jtown.eu>
2021-03-07 16:24:16 +0000

    reworking stencil: applying const_caching to Stencil.get_raw_stencil
           added .dup
    
         extending documentation

--- lib/cotcube-swapseeker/models/stencil.rb
+++ lib/cotcube-swapseeker/models/stencil.rb
@@ -8,57 +8,56 @@ module Cotcube
       attr_reader   :swaps
 
 
+      # Class method that loads the (latest) obligatory stencil for given interval and type.
+      # These raw stencils are located in /var/cotcube/swapseeker/stencils
+      #
+      # Current daily stencils contain dates from 2020-01-01 to 2023-12-21
+      #
       def self.provide_raw_stencil(type:, interval: :daily, version: nil)
-        file_base = "/var/cotcube/swapseeker/stencils/stencil_#{interval.to_s}_#{type.to_s}.csv_"
-        if Dir["#{file_base}?*"].empty?
-          raise ArgumentError, "Could not find any stencil for interval #{interval} and type #{type}. Check #{file_base} manually!"
-        end
-        if version.nil? # use latest available version if not given
-          file = Dir["#{file_base}?*"].sort.last
-        else
-          file = "#{file_base}#{version}"
-          unless File.exist? file
-            raise ArgumentError, "Cannot open stencil from non-existant file #{file}."
+        loading = lambda do |typ|
+          file_base = "/var/cotcube/swapseeker/stencils/stencil_#{interval.to_s}_#{typ.to_s}.csv_"
+          if Dir["#{file_base}?*"].empty?
+            raise ArgumentError, "Could not find any stencil matching interval #{interval} and type #{typ}. Check #{file_base} manually!"
           end
-        end
-        CSV.read(file).map{|x| {datetime: CHICAGO.parse(x.first), x: x.last.to_i.freeze } }
-      end
-
-      def add_boundaries
-        @swaps.each do |swap|
-          p swap
-        end
-      end
-
-      def show_members
-        @swaps.each_with_index do |swap, index|
-          swap[:members].each do |member|
-            puts "#{index}\t#{member[:datetime].strftime('%a %Y-%m-%d')} #{member[:contract]
-               }\t#{member[:high]}\t#{member[:x]}\t#{member[:dx].round(4)}\t#{member[:i]}"
+          if version.nil? # use latest available version if not given
+            file = Dir["#{file_base}?*"].sort.last
+          else
+            file = "#{file_base}#{version}"
+            unless File.exist? file
+              raise ArgumentError, "Cannot open stencil from non-existant file #{file}."
+            end
           end
+          CSV.read(file).map{|x| {datetime: CHICAGO.parse(x.first), x: x.last.to_i.freeze } }
         end
+        unless const_defined? :RAW_STENCILS
+          const_set :RAW_STENCILS, { daily:
+                                     { full: loading.call( :full).freeze,
+                                       rtc:  loading.call( :rtc).freeze
+          }.freeze
+          }.freeze
+        end
+        RAW_STENCILS[interval][type]
       end
-
 
       def initialize(
-        range: nil,
+        range: nil,                 # used to shrink the stencil size, accepts String or Date
         interval:,
         swap_type:,
-        ranges: nil,
+        ranges: nil,                # currently not used, prepared to be used in connection intraday
         contract: nil,
-        today: nil,
-        now: nil,
+        today: nil,                 # today and now are mutually exclusive, being used with dailies in the first, with intraday in latter case
         debug: false,
-        version: nil,
+        version: nil,               # when referring to a specicic version of the stencil
         timezone: CHICAGO,
+        stencil: nil,               # instead of loading, use this data
         config: init
       )
-        @debug    = debug
-        @interval = interval
+        @debug     = debug
+        @interval  = interval
         @swap_type = swap_type
-        @swaps    = nil
-        @contract = contract
-        step =  case interval
+        @swaps     = []
+        @contract  = contract
+        step =  case @interval
                 when :hours, :hour; 1.hour
                 when :quarters, :quarter; 15.minutes
                 else; 1.day
@@ -69,64 +68,83 @@ module Cotcube
           unless range.nil?
             starter = range.begin.is_a?(String) ? timezone.parse(range.begin) : range.begin
             ender   = range.  end.is_a?(String) ? timezone.parse(range.  end) : range.  end
-          end
-
-          stencil_type = case swap_type
-                   when :rth
-                     :full
-                   when :rthc
-                     :rtc
-                   else
-                     swap_type
-                   end
-          @base = Stencil.provide_raw_stencil(type: stencil_type, interval: :daily, version: version)
-
-          # fast forward to prev trading day
-          @today = today || Date.today
-          best_match = @base.select{|x| x[:datetime].to_date <= @today}.last[:datetime]
-          @today  = best_match
-
-          offset = @base.map{|x| x[:datetime]}.index(@today)
-
-          @base
-            .map!
-            .each_with_index{|d,i| { datetime: d[:datetime].freeze, x: (offset - i ).freeze } }
-          @base
-            .select!{|d| (d[:datetime] >= starter and d[:datetime] <= ender) } unless range.nil?
-        else
-          raise RuntimeError, "'interval: #{interval}' was provided, what does not match anything this tool can handle (currently :quarters, :hours, :days)."
         end
+
+        stencil_type = case swap_type
+                       when :rth
+                         :full
+                       when :rthc
+                         :rtc
+                       else
+                         swap_type
+                       end
+        # TODO: Check / warn / raise whether stencil (if provided) is a proper data type
+        raise ArgumentError, "Stencil should be nil or Array" unless [NilClass, Array].include? stencil.class
+        raise ArgumentError, "Each stencil members should contain at least :datetime and :x" unless stencil.nil? or
+          stencil.map{|x| ([:datetime, :x] - x.keys).empty? and x[:datetime].is_a?(ActiveSupport::TimeWithZone) and x[:x].is_a?(Integer)}.reduce(:&)
+
+        base = stencil || Stencil.provide_raw_stencil(type: stencil_type, interval: :daily, version: version)
+
+        # fast forward to prev trading day
+        @today = today || Date.today
+        best_match = base.select{|x| x[:datetime].to_date <= @today}.last[:datetime]
+        @today  = best_match
+
+        offset = base.map{|x| x[:datetime]}.index(@today)
+
+        # apply offset to stencil, so zero will match today (or what was provided as 'today')
+        @base = base.map.
+          each_with_index{|d,i| { datetime: d[:datetime].freeze, x: (offset - i ).freeze } }
+        # if range was given, shrink stencil to specified range
+        @base.select!{|d| (d[:datetime] >= starter and d[:datetime] <= ender) } unless range.nil?
+      else
+        raise RuntimeError, "'interval: #{interval}' was provided, what does not match anything this tool can handle (currently :days, :dailies, :synthetic)."
       end
+    end
 
-      def zero
-        @zero ||=  @base.select{|x| x[:x].zero? }.first
-      end
+    def dup
+      Stencil.new(
+        debug:      @debug,
+        interval:   @interval,
+        swap_type:  @swap_type,
+        today:      @today,
+        contract:   @contract,
+        stencil:    @base.map{|x| x.dup}
+      )
+    end
 
-      def stencil
-        @base
-      end
 
-      def swaps=(swaps)
-        puts "WARNING: overwriting existing swaps!".light_yellow unless @swaps.nil?
-        @swaps = swaps
-      end
+    def zero
+      @zero ||=  @base.find{|x| x[:x].zero? }
+    end
 
-      def inspect
-        "#<Stencil:0x0815> @contract:#{@contract} @interval:#{@interval} @swaps:#{@swaps.size} @base:#{@base.size} today:#{@today}" +
-          @swaps.each_with_index.map { |swap,i|
-          len = swap[:members].first[:x] + 1
-          mem = swap[:members].size
-          guess_stencil  = Stencil.
-            new( interval: :synthetic, swap_type: @swap_type, debug: @debug, today: @today, contract: @contract ).
-            stencil.
-            select{|s| s[:x] < 0}.
-            first
+    def stencil
+      @base
+    end
 
-          puts "GS: #{guess_stencil}"  if @debug
-          rat = swap[:rating]
-          col = (rat > 30) ? :magenta : (rat > 15) ? :light_magenta : (rat > 6) ? :light_green : swap[:rated] ? :light_yellow : :yellow
+    def swaps=(swaps)
+      puts "WARNING: overwriting existing swaps!".light_yellow unless @swaps.nil?
+      @swaps = swaps
+    end
 
-          "\n\t\tswap #{i}: len:#{    format '%3d', len
+    # convenient output of stencil data and attached swaps (if any)
+    def inspect
+      "#<Stencil:0x0815> @contract:#{@contract} @interval:#{@interval} @swaps:#{@swaps.size} @base:#{@base.size} today:#{@today}" +
+        @swaps.each_with_index.map { |swap,i|
+        len = swap[:members].first[:x] + 1
+        mem = swap[:members].size
+        # the guess stencil is quite overloaded, but I don't have access to a stencil with future dates yet at this point
+        guess_stencil  = Stencil.
+          new( interval: :synthetic, swap_type: @swap_type, debug: @debug, today: @today, contract: @contract ).
+          stencil.
+          select{|s| s[:x] < 0}.
+          first
+
+        puts "GS: #{guess_stencil}"  if @debug
+        rat = swap[:rating]
+        col = (rat > 30) ? :magenta : (rat > 15) ? :light_magenta : (rat > 6) ? :light_green : swap[:rated] ? :light_yellow : :yellow
+
+        "\n\t\tswap #{i}: len:#{    format '%3d', len
                          } rating:#{  format '%2d', swap[:rating]
                          } mem:#{     format '%2d', mem
                          } deg:#{     format '%4.2f', swap[:deg]
@@ -139,6 +157,7 @@ module Cotcube
         }.join
       end
 
+      # calls the calculus for each attached swap
       def calculus(number=nil)
         if @swaps.empty?
           puts "No swaps in stencil!".light_red
@@ -149,31 +168,26 @@ module Cotcube
               swap[:calculus].call(members: swap[:members].map{|x| x[:x]} )
             end
           else
-            puts "Presenting swap [#{n}]".light_green
-            @swaps[n][:calculus].call(members: @swaps[n][:members].map{|x| x[:x]} )
+            puts "Presenting swap [#{number}]".light_green
+            @swaps[number][:calculus].call(members: @swaps[number][:members].map{|x| x[:x]} )
           end
         end
       end
 
-
-      def inject_base( contract:, base: nil, range: (0..-1))
+      def inject_base(contract:, base: nil, range: (0..-1))
         integer_range = ((range.is_a?(Range)) and ((range.begin.is_a?(Integer)) || range.end.is_a?(Integer)))
         sym = Cotcube::Bardata.get_id_set(contract: contract)
         base ||= Cotcube::Bardata.provide contract: contract, interval: @interval, filter: @filter, range: (integer_range ? nil : range)
         base = base[range] if integer_range
         offset = 0
         base.each_with_index do |_,i|
-          #puts "BASE: #{base[i][:datetime]}" if @debug
-          #puts  "#{stencil[i+offset][:datetime]} < #{base[i][:datetime]}" if @debug
           begin
-            while @base[i+offset][:datetime] < base[i][:datetime]
-              #puts "skipping #{@base[i+offset][:datetime]}".yellow if @debug
-              offset += 1
-            end
-          rescue
-            # I dont know why but for holidays it fails
+            offset += 1 while @base[i+offset][:datetime] < base[i][:datetime]
+          rescue Exception => ex
+            puts("ERROR processing #{i} + #{offset} on #{@base[i+offset]} vs #{base[i]}")
+            puts("======= ERROR: '#{ex.class}', MESSAGE: '#{ex.message}'")
             puts "WARNING: Holiday found. skipping!".light_yellow
-            #return { upper: [], lower: [] }
+            next
           end
           if @base[i+offset][:datetime] > base[i][:datetime]
             puts "skipping #{base[i]}".light_yellow if @debug
@@ -181,13 +195,25 @@ module Cotcube
             next
           end
           j = i + offset
-          #puts "#{i}\t#{offset}" if @debug
-          #puts "ST: #{stencil[j][:datetime]}" if @debug
-          @base[j] = @base[j].merge(base[i]) if @base[j][:volume].nil?
-          #puts "->: #{@base[j]}\n\n" if @debug
+          # the following line does the actual injection
+          # NOTE: for each node, data is only injected, if it only contains the
+          #       basice keys :datetime and :x
+          @base[j] = @base[j].merge(base[i]) if @base[j].keys.size == 2
         end
       end
+
+      def show_members
+        @swaps.each_with_index do |swap, index|
+          swap[:members].each do |member|
+            puts "#{index}\t#{member[:datetime].strftime('%a %Y-%m-%d')} #{member[:contract]
+               }\t#{member[:high]}\t#{member[:x]}\t#{member[:dx].round(4)}\t#{member[:i]}"
+          end
+        end
+      end
+
     end
+
   end
+
 end
 
