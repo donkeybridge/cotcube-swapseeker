2ba7becaf2625ce34769a5ab1729174b232e111c
Benjamin L. Tischendorf <donkeybridge@jtown.eu>
2021-03-27 21:41:38 +0000

    init.rb: added (default-) format to symbols.csv

--- lib/cotcube-swapseeker/init.rb
+++ lib/cotcube-swapseeker/init.rb
@@ -9,9 +9,9 @@ module Cotcube
         SYMBOL_EXAMPLES
       else
         CSV
-          .read(config[:symbols_file], headers: %i{ id symbol ticksize power months type bcf reports name})
+          .read(config[:symbols_file], headers: %i{ id symbol ticksize power months type bcf reports format name})
           .map{|row| row.to_h }
-          .map{|row| [ :ticksize, :power, :bcf ].each {|z| row[z] = row[z].to_f}; row }
+          .map{|row| [ :ticksize, :power, :bcf ].each {|z| row[z] = row[z].to_f}; row[:format] = "%#{row[:format]}f"; row }
           .reject{|row| row[:id].nil? }
           .tap{|all| all.select!{|x| x[:type] == type} unless type.nil? }
           .tap { |all| all.select! { |x| x[:symbol] == symbol } unless symbol.nil? }
