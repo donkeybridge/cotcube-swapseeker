729f8ea6dd5ee017872db2f70413f4e8a61ce44a
Benjamin L. Tischendorf <donkeybridge@jtown.eu>
2021-03-27 21:47:25 +0000

    asset.rb: slightly improved by adding default format to symbols.csv

--- lib/cotcube-swapseeker/models/asset.rb
+++ lib/cotcube-swapseeker/models/asset.rb
@@ -2,21 +2,22 @@ module Cotcube
   module SwapSeeker
     class Asset
 
-      attr :symbol
-
       include Mongoid::Document
       has_many :contracts, class_name: 'Contract', inverse_of: :asset
       field :s, as: :symbol, type: String
+
       validates_uniqueness_of :s
 
-      def sym;      @sym ||= Cotcube::Bardata.get_id_set(symbol: @symbol); end
-      def ticksize; @sym[:ticksize];                                end
-      def power;    @sym[:power];                                   end
-      def months;   @sym[:months];                                  end
-      def name;     @sym[:name];                                    end
-      def to_tick(x); (x / ticksize).round * ticksize;              end
-      def fmt(x);   true; end
+      def sym;      @sym ||= Cotcube::Bardata.get_id_set(symbol: s); end
+      def ticksize; self.sym[:ticksize];                             end
+      def power;    self.sym[:power];                                end
+      def months;   self.sym[:months];                               end
+      def name;     self.sym[:name];                                 end
+      def to_tick(x); (x / ticksize).round * ticksize;               end
 
+      def fmt(number=nil)
+        number.nil? ? self.sym[:format] : format(self.sym[:format], number)
+      end
 
       def self.method_missing(method, *args, &block)
         m = method.to_s.upcase
