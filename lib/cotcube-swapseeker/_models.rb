10b974a90a38779b158442a9ecbc42f72a8daed8
Benjamin L. Tischendorf <donkeybridge@jtown.eu>
2021-03-27 17:56:28 +0000

    _models.rb: split DB and non-DB models for better overview, renamed dots to bars

--- lib/cotcube-swapseeker/_models.rb
+++ lib/cotcube-swapseeker/_models.rb
@@ -2,7 +2,21 @@ Mongoid.load!("./mongoid.yml",:development)
 Mongoid.logger.level       = Logger::WARN
 Mongo::Logger.logger.level = Logger::WARN
 
-%i[ stencil day asset contract member swap].each do |model|
+# non-DB models
+
+%i[stencil].each do |model|
+  require_relative "#{__dir__}/models/#{model}"
+end
+
+# DB models
+
+# Day has many swaps
+# Contract has many swaps
+#      belongs to asset
+# Swap belongs to day and contract
+#      embeds members, line and guess
+
+%i[bar line day asset contract member swap].each do |model|
   require_relative "#{__dir__}/models/#{model}" 
 end
 
