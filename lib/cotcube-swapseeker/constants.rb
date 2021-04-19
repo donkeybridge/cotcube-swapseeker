d74e39ca535e31ea09f4967697cf9f7ace1a2771
Benjamin L. Tischendorf <donkeybridge@jtown.eu>
2021-04-19 13:42:46 +0000

    constants.rb: added reverse mapping to MONTHS, so now nr->letter and letter->nr is supported)

--- lib/cotcube-swapseeker/constants.rb
+++ lib/cotcube-swapseeker/constants.rb
@@ -15,6 +15,16 @@ module Cotcube
                        'N' => :cyan,  'Q' => :magenta, 'U' => :light_magenta,
                        'V' => :blue,  'X' => :red,     'Z' => :light_red }.freeze
 
+    MONTHS         = { 'F' => 1,  'G' =>  2, 'H' =>  3,
+                       'J' => 4,  'K' =>  5, 'M' =>  6,
+                       'N' => 7,  'Q' =>  8, 'U' =>  9,
+                       'V' => 10, 'X' => 11, 'Z' => 12,
+                        1 => 'F',  2 => 'G',  3 => 'H',
+                        4 => 'J',  5 => 'K',  6 => 'M',
+                        7 => 'N',  8 => 'Q',  9 => 'U',
+                       10 => 'V', 11 => 'X', 12 => 'Z' }.freeze
+
+
     CHICAGO  = Time.find_zone('America/Chicago')
 
   end
