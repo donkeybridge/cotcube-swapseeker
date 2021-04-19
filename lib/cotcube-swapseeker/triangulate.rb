b024fd9a5c04fc7644f3575c7527af08382652c2
Benjamin L. Tischendorf <donkeybridge@jtown.eu>
2021-04-19 18:06:06 +0000

    triangulate: Improved documentation, adopeted new Swap model approach

--- lib/cotcube-swapseeker/triangulate.rb
+++ lib/cotcube-swapseeker/triangulate.rb
@@ -5,7 +5,7 @@ module Cotcube
           contract: nil,        # contract actually isnt needed to triangulation, but allows much more convenient output
           side:,                # :upper or :lower
           base:,                # a Stencil with injected base
-          range: (0..-1),       # range is relative to base.
+          range: (0..-1),       # range is relative to base
           max: 90,              # the range which to scan for swaps goes from deg 0 to max
           debug: false,
           format: '% 5.2f',
@@ -16,14 +16,23 @@ module Cotcube
         raise ArgumentError, "'0 < max < 90, but got '#{max}'" unless max.is_a? Numeric and 0 < max and max <= 90
         raise ArgumentError, 'need :side either :upper or :lower for dots' unless [:upper, :lower].include? side
 
+        ###########################################################################################################################
         # init some helpers
-        high = side == :upper
-        first = base.find{|x|  not x[:high].nil? }
-        zero = base.select{|x| x[:x].zero? }
+        #
+        high       = side == :upper
+        first      = base.to_a.find{|x|  not x[:high].nil? }
+        zero       = base.select{|x| x[:x].zero? }
         raise ArgumentError, "Inappropriate base, it should contain ONE :x.zero, but contains #{zero.size}." unless zero.size==1
-        zero = zero.first
+        zero       = zero.first
+        contract ||= zero.contract
+        sym = Cotcube::Bardata.get_id_set(contract: contract)
+        ticksize = sym[:ticksize]  / sym[:bcf] # need to adjust, as we are working on barchart data, not on exchange data !!
 
-        # prepare nase (i.e. dupe the original, create proper :y, and reject unneeded items
+
+
+        ###########################################################################################################################
+        # prepare base (i.e. dupe the original, create proper :y, and reject unneeded items
+        #
         base = base.
           map { |x|
             y = x.dup
@@ -31,13 +40,14 @@ module Cotcube
                      (y[:high] - zero[:high]).round(8) :
                      (zero[:low] - y[:low]).round(8)
                     ) unless y[:high].nil?
-            #[ :open, :close, :type ].map{|z| y.delete(z) }
             y
           }.
-          reject{|b| b[:datetime] < first[:datetime] or b[:x] < 0 or b[:y].nil?}[range]
+          reject{|b| b.nil? or b[:datetime] < first[:datetime] or b[:x] < 0 or b[:y].nil?}[range]
 
-        contract ||= base.first[:contract]
-        sym = Cotcube::Bardata.get_id_set(contract: contract)
+
+        ###########################################################################################################################z
+        # only if (and only if) the range portion above change the underlying base
+        #   the offset has to be fixed for :x and :y
 
         unless range == (0..-1)
           puts "adjusting range to '#{range}'".light_yellow if debug
@@ -46,27 +56,36 @@ module Cotcube
           base.map!{|b| b[:x] -= offset_x; b[:y] -= offset_y  ; b}
         end
 
-        ticksize = sym[:ticksize]  / sym[:bcf] # need to adjust, as we are working on barchart data, not on exchange data !!
 
-        # simplifying output with a lambda
+        ###########################################################################################################################
+        # LAMBDA no1: simplifying DEBUG output
+        #
         present = lambda {|z| "#{z[:datetime].strftime("%a, %Y-%m-%d %H:%M")
                          }  x: #{format '%-4d', z[:x]
                          } dx: #{format '%-4d', (z[:dx].nil? ? z[:x] : z[:dx]) 
-                            }, #{high ? "high" : "low"
+                             } #{high ? "high" : "low"
                             }: #{format format, z[high ? :high : :low]
-                         }, i: #{(format '%4d', z[:i]) unless z[:i].nil?}" 
+                          } i: #{(format '%4d', z[:i]) unless z[:i].nil?}"
         }
 
-        # another lambda to find the x-fit, for each member where it is zero.
+        ###########################################################################################################################
+        # LAMBDA no2: all members except the pivot itself now most probably are too far to the left
+        #             finalizing tries to get the proper dx value for them
+        #
+        #             TODO: Turn the hash into a Swap object
+        #
+        #             there is another (currently deactivated) tweak here, that helps guessing
+        #
         finalize = lambda do |res|
-          res.each do |r|
-            r[:members].each  do |m|
+          res.map do |r|
+            r.members.each  do |m|
               next if m[:yy].nil? or m[:yy].zero?
 
               diff = (m[:x] - m[:dx]).abs / 2.0
               m[:dx] = m[:x] + diff
               # it employs another binary-search
               while m[:yy].round(8) != 0
+                print '.' if debug
                 m[:yy] = shear_to_deg(deg: r[:deg], base: [ m ] ).first[:yy]
                 diff /= 2.0
                 if m[:yy] > 0
@@ -76,11 +95,15 @@ module Cotcube
                 end
               end
               m[:yy] = m[:yy].abs.round(8)
-            end # r[:members]
+            end # r.members
+            puts 'done!'.light_yellow if debug
 
-            r[:members].each {|x| puts "finalizing #{x}" } if debug
+            r.members.each {|x| puts "finalizing #{x}".magenta } if debug
+            ## The transforming part
 
-            if allow_sub
+            ## The guessing part
+
+            if false and allow_sub
               puts "going sub".light_yellow if debug
               sub_range = (r[:members][0][:i]..r[:members][-2][:i])
               sub_x     =  r[:members][-2][:x]
@@ -108,22 +131,24 @@ module Cotcube
 	      }
 		puts "offsets x:#{sub_x}\ty: #{sub_y}".light_green if debug
 	    end  # if allow_sub
-
+            r
 	  end    # res
 	end      # lambda
 
 
-	# a third lambda, the actual 'function' to retrieve the slope
+        ###########################################################################################################################
+	# LAMDBA no3:  the actual 'function' to retrieve the slope
+        #
 	get_slope = lambda do |b|
 	  if debug
-	    puts "SETTING BASE: ".light_green
+            puts "in get_slope ... SETTING BASE: ".light_green
 	    puts "Last:\t#{present.call  b.last}"
 	    puts "First:\t#{present.call b.first}"
 	  end
 	  members = [ b.last[:i] ]
 	  loop do
-	    current_slope   = detect_slope(base: b, ticksize: ticksize, format: format, side: side, debug: debug)
-	    current_members = current_slope[:members]
+	    current_slope   = detect_slope(base: b, ticksize: ticksize, format: sym[:format], side: side, debug: debug)
+            current_members = current_slope.members
 	      .map{|dot| dot[:i]}
 	    new_members = current_members - members
 	    puts "New members: #{new_members} as of #{current_members} - #{members}" if debug
@@ -134,7 +159,7 @@ module Cotcube
 	      if mem_sorted[1] == mem_sorted[0] + 1
                 b2 = b[mem_sorted[1]..mem_sorted[-1]].map{|x| x.dup; x[:dx] = nil; x}
                 alternative_slope = get_slope.call(b2)
-		alternative = alternative_slope[:members].map{|bar| bar[:i]}
+                alternative = alternative_slope.members.map{|bar| bar[:i]}
 		if (mem_sorted[1..-1] - alternative).empty?
 		  current_slope = alternative_slope
 		  members = alternative
@@ -151,9 +176,8 @@ module Cotcube
               end
               members.sort_by{|i| -i}.each do |x|
                 puts "#{range}\t#{present.call(b[x])}" if debug
-                # current_slope[:members] = []
-                current_slope[:members] << b[x] unless current_slope[:members].map{|x| x[:datetime]}.include? b[x][:datetime]
-                current_slope[:members].sort_by!{|x| x[:datetime]}
+                current_slope.members << b[x] unless current_slope.members.map{|x| x[:datetime]}.include? b[x][:datetime]
+                current_slope.members.sort_by!{|x| x[:datetime]}
               end
               return current_slope
 
@@ -169,33 +193,46 @@ module Cotcube
         end # of lambda
 
 
-
-
+        ###########################################################################################################################
+        # after declaring lambdas, the rest is quite few code
+        #
         base.each_index.map{|i| base[i][:i] = -base.size + i } 
-        old_base = base.dup
+        current_range = (0..-1)                                                                                         # RANGE   set
+        current_slope = Swap.new                                                                                        # SLOPE   reset
+        current_base = base[current_range]                                                                              # BASE    set
+        current_results = [ ]                                                                                           # RESULTS reset
+        while current_base.size >= 5                                                                                    # LOOP
 
-        current_range = (0..-1)
-        current_slope = { members: [] }
-        current_base = base[current_range]
-        current_results = [ ]
-        while current_base.size >= 5
-          while current_base.size >= 5 and current_slope[:members].size < min_members
-            puts current_range.to_s.light_yellow if debug
-            current_slope = get_slope.call(current_base)
-            next_i = current_slope[:members][-2]
-            current_range = ((next_i.nil? ? -2 : next_i[:i])+1..-1)
-            current_base = base[current_range]
-            STDIN.gets if debug
+          puts '-------------------------------------------------------------------------------------' if debug
+
+          while current_base.size >= 5 and current_slope.members.size < min_members
+            puts "---- #{current_base.size} #{current_range.to_s.light_yellow} ------" if debug
+            current_slope = get_slope.call(current_base)                                                                # SLOPE   call
+            next_i  = current_slope.members[-2]
+            current_range = ((next_i.nil? ? -2 : next_i[:i])+1..-1)                                                     # RANGE   adjust
+            current_base = base[current_range]                                                                          # BASE    adjust
+            if debug
+              print 'Hit <enter> to continue...'
+              STDIN.gets
+            end
           end
-          puts "#{current_slope}" if debug# is a swap
-          current_results << current_slope if current_slope 
-          current_slope = { members: [] }
+          puts "Current slope: ".light_yellow + "#{current_slope}" if debug
+          current_results << current_slope if current_slope                                                             # RESULTS add
+          current_slope = Swap.new                                                                                      # SLOPE   reset
         end
-        # current_results.select!{|x| x[:members].size >= min_members }
-        current_results.select!{|x|  x[:members][0..-2].size > min_members or x[:members][0..-2].one_by_one{|a,b| (a[:x] - b[:x]).abs > 1 }.reduce(:|)}
+        current_results.select!{|x|  x.members.size >= min_members }
+
+        # Adjust all members (except pivot) to fit the actual dx-value
         finalize.call(current_results)
-        current_results
       end
+    end
+  end
+end
+
+module Cotcube
+  module SwapSeeker
+    module Helpers
+      module_function :triangulate
     end
   end
 end
